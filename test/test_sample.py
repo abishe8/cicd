cac0fd70f1b94b89d4303d642272068f3a4fa554
Abhishek <kbabhishek99@gmail.com>
2025-02-12 14:19:48 +0530

    saved the files

---
+++ test/test_sample.py
@@ -0,0 +1,10 @@
+from src.sample import add,sub
+
+def test_add(a,b):
+    assert add(3,4) == 7
+    assert add(5,4) == 9
+    assert add(2,8) == 10
+
+def test_sub(a,b):
+    assert sub(5,2) == 3
+    assert add(14,6) == 8    

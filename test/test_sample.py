15d77e06d9d6efc42e48069705cefc922fa42b9e
Abhishek <kbabhishek99@gmail.com>
2025-02-12 14:50:47 +0530

    corrected the code

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,12 +1,12 @@
-from src.sample import add,sub
+from src.sample import add, sub
 
-def test_add(a,b):
-    assert add(3,4) == 7
-    assert add(5,4) == 9
-    assert add(2,8) == 10
+def test_add():
+    assert add(3, 4) == 7
+    assert add(5, 4) == 9
+    assert add(2, 8) == 10
 
-def test_sub(a,b):
-    assert sub(5,2) == 3
-    assert add(14,6) == 8    
+def test_sub():
+    assert sub(5, 2) == 3
+    assert sub(14, 6) == 8  # Fixed the function call from 'add' to 'sub'
 
 #just to trigger the github actions
